6e722379be0a89ad696241325682df0e7375a646
Alex McKirdy <alexmckirdy@gmail.com>
2016-11-11 19:01:42 +1300

    Papermaker: commenting and formatting in main.py

--- Papermaker/main.py
+++ Papermaker/main.py
@@ -5,32 +5,38 @@ import random
 import math
 
 # Custom methods
-from methods import gen_squareangles
+from methods import * 
 from colour import *
 
 # filename = random.randint(10000000, 99999999)
 size = (2560, 1600)  # width, height
 
 
+
 def create(points, color=None):
+    # Set base colour
     if color is not None:
         h, s, l = color
     else:
         h, s, l = random_hsl()
-
+    
+    # Create an initial, empty PIL image
     img = Image.new("RGB", size, hsl(h, s, l))
-
     draw = ImageDraw.Draw(img)
-
+    
+    # Draw each polygon
     for p in points:
         draw.polygon(p, modify_hsl(h, s, l, factor=15))
+    
+    # Save (or display)
     # img.show
     img.save(str(datetime.datetime.now()) + ".PNG", "PNG")
 
 
+
 def __main__():
     for i in range(10):
-        create(gen_squareangles(size), random_hsl() )
+        create(gen_3(size), random_hsl() )
         print (i)
 
 
